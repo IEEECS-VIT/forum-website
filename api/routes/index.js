99d9c2e7e1d3ed78c74ff1da8353dff7b66dd501
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2016-04-26 20:31:47 +0530

    Added the user routes, with commented session code

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var mongo = require('mongodb');
+var mongoClient = mongo.MongoClient;
+var url = "mongodb://Vishwajeet:310toyuma@ds025409.mlab.com:25409/ieeecsforum";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  mongoClient.connect(url,function(err,db){
+    if(err){
+      res.json(err);
+    } else{
+      var collection = db.collection('userCollection');
+      collection.find().toArray(function(err,data){
+        res.json(data);
+      });
+    }
+  });
 });
 
 module.exports = router;
